7b0628771d0d0d02c80fe1a80deb176881a740b9
luqmanulhakim96 <hakim9608@gmail.com>
2021-07-29 09:40:09 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,86 +36,87 @@ class HomeController extends Controller
      */
     public function index()
     {
-      $student = Student::where('user_id', Auth::user()->id)->first();
-      $announcement = Announcement::orderBy('id', 'DESC')->get();
-      $name= Auth::user()->name;
-      // dd($name);
-      $splitName = explode(' ', $name); // Restricts it to only 2 values, for names like Billy Bob Jones
+        $student = Student::where('user_id', Auth::user()->id)->first();
+        $announcement = Announcement::orderBy('id', 'DESC')->get();
+        $name = Auth::user()->name;
+        // dd($name);
+        $splitName = explode(' ', $name); // Restricts it to only 2 values, for names like Billy Bob Jones
 
-      $first_name = $splitName[0];
-      $last_name = !empty($splitName[1]) ? $splitName[1] : ''; // If last name doesn't exist, make it empty
-
-
+        $first_name = $splitName[0];
+        $last_name = !empty($splitName[1]) ? $splitName[1] : ''; // If last name doesn't exist, make it empty
 
 
-      //check student id
-      $student_checker = Student::where('user_id', Auth::user()->id)->count();
-
-      if($student_checker == 0){
-        //create student id
-        $student = new Student();
-
-        $student->user_id = Auth::user()->id;
-        $student->save();
-
-        //fetch student id
-        $student_id = Auth::user()->student->id;
-
-        //checking table leaderboard sama ada dah daftar ke blum
-        $find_leaderboard = Leaderboard::where('student_id', $student_id)->count();
 
 
-        //kalau 0 tak daftar, klau 1 dah daftar
-        if($find_leaderboard == 0){
+        //check student id
+        $student_checker = Student::where('user_id', Auth::user()->id)->count();
 
-          //create leader board
-          $leaderboard = new Leaderboard();
+        if ($student_checker == 0) {
+            //create student id
+            $student = new Student();
 
-          //initialize data
-          $leaderboard->student_id = $student_id;
-          $leaderboard->scores = 0;
-          $leaderboard->save();
+            $student->user_id = Auth::user()->id;
+            $student->save();
+
+            //fetch student id
+            $student_id = Auth::user()->student->id;
+
+            //checking table leaderboard sama ada dah daftar ke blum
+            $find_leaderboard = Leaderboard::where('student_id', $student_id)->count();
+
+
+            //kalau 0 tak daftar, klau 1 dah daftar
+            if ($find_leaderboard == 0) {
+
+                //create leader board
+                $leaderboard = new Leaderboard();
+
+                //initialize data
+                $leaderboard->student_id = $student_id;
+                $leaderboard->scores = 0;
+                $leaderboard->save();
+            }
         }
-      }
 
-      // dd(count($splitName));
+        // create student id
+        if ($student) {
+            //track student progress
+            $student_id = Auth::user()->student->id;
+            $lesson_progress = Student_Lesson::where('student_id', $student_id)->count();
+            $lesson_progress = ($lesson_progress / 10) * 100;
 
+            $quiz_progress = Student_Quiz::where('result_status', "Lulus")->where('student_id', $student_id)->distinct('quiz_id')->count();
+            $quiz_progress = ($quiz_progress / 10) * 100;
 
+            $game_progress = Student_Game::where('student_id', $student_id)->distinct('game_id')->count();
+            $game_progress = ($game_progress / 10) * 100;
 
-      // create student id
-      if($student){
-        //track student progress
-        $student_id = Auth::user()->student->id;
-        $lesson_progress = Student_Lesson::where('student_id', $student_id)->count();
-        $lesson_progress = ($lesson_progress/10) * 100;
+            auth()->user()->lesson_progress = $lesson_progress;
+            auth()->user()->quiz_progress = $quiz_progress;
+            auth()->user()->game_progress = $game_progress;
+            auth()->user()->save();
+        } else {
+            $student_id = Student::create([
+                'user_id' => Auth::user()->id,
+            ]);
 
-        $quiz_progress = Student_Quiz::where('result_status', "Lulus")->where('student_id', $student_id)->distinct('quiz_id')->count();
-        $quiz_progress = ($quiz_progress/10) * 100;
-
-        $game_progress = Student_Game::where('student_id', $student_id)->distinct('game_id')->count();
-        $game_progress = ($game_progress/10) * 100;
-
-
-        return view('home', compact('announcement','lesson_progress','quiz_progress', 'game_progress', 'splitName'));
-      }else {
-        $student_id = Student::create([
-          'user_id' => Auth::user()->id,
-        ]);
-
-        //track student progress
-        $lesson_progress = Student_Lesson::where('student_id', $student_id)->count();
-        $lesson_progress = ($lesson_progress/10) * 100;
-
-        $quiz_progress = Student_Quiz::where('result_status', "Lulus")->where('student_id', $student_id)->distinct('quiz_id')->count();
-        $quiz_progress = ($quiz_progress/10) * 100;
-
-        $game_progress = Student_Game::where('student_id', $student_id)->distinct('game_id')->count();
-        $game_progress = ($game_progress/10) * 100;
+            //track student progress
+            $lesson_progress = Student_Lesson::where('student_id', $student_id)->count();
+            $lesson_progress = ($lesson_progress / 10) * 100;
 
 
-        return view('home', compact('announcement','lesson_progress','quiz_progress','game_progress', 'splitName'));
-      }
+            $quiz_progress = Student_Quiz::where('result_status', "Lulus")->where('student_id', $student_id)->distinct('quiz_id')->count();
+            $quiz_progress = ($quiz_progress / 10) * 100;
+
+            $game_progress = Student_Game::where('student_id', $student_id)->distinct('game_id')->count();
+            $game_progress = ($game_progress / 10) * 100;
+
+            auth()->user()->lesson_progress = $lesson_progress;
+            auth()->user()->quiz_progress = $quiz_progress;
+            auth()->user()->game_progress = $game_progress;
+            auth()->user()->save();
+        }
+
+        return view('home', compact('announcement', 'lesson_progress', 'quiz_progress', 'game_progress', 'splitName'));
     }
-
-
 }
